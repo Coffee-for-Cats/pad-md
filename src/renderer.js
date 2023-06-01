d739c0e3cdaa5e0772fb56a68566416ed33bd1f6
Lucas S. Silva <57202598+Coffee-for-Cats@users.noreply.github.com>
2023-06-01 17:43:19 -0300

    cleaning some things up

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,6 @@
 const pad = {
     filePath: "",
-    editMode: true,
+    //editMode: true,
     _rawText: "",
     _contentPlacer: null,
 
@@ -18,12 +18,12 @@ const pad = {
     }
 }
 
-function render() {
+function render(editMode = 'view') {
     
     let displayContent = document.createElement('pre');
     displayContent.id = "content-placer";
 
-    if (pad.editMode) {
+    if (editMode == 'edit') {
         displayContent.contentEditable = "plaintext-only";
         displayContent.textContent = pad.getRawText();
         displayContent.className += 'editing';
@@ -32,7 +32,7 @@ function render() {
         document.getElementById('buttonView').className = "nonBoldButton";
 
     // if I am entering the view mode
-    } else if (pad.editMode == false) {
+    } else if (editMode == 'view') {
         
         //keep the rawText up to date!
         let editing = document.querySelector('.editing');
@@ -85,14 +85,3 @@ document.addEventListener('drop', async function openFile(e) {
     
     pad.setRawText(fileContent);
 })
-
-//html buttons
-//changes the editMode and render the text to screen
-function editButton() {
-    pad.editMode = true;
-    render()
-}
-function viewButton() {
-    pad.editMode = false;
-    render()
-}

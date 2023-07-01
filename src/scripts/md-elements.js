f88be8cdd7b000db678df76fe9298a045fd1fe74
Lucas S. Silva <57202598+Coffee-for-Cats@users.noreply.github.com>
2023-07-01 00:16:33 -0300

    working bold! Needs cleaning

--- src/scripts/md-elements.js
+++ src/scripts/md-elements.js
@@ -3,22 +3,22 @@ const blockElements = {
     //               the function argument is the rest of the content.
     '#': (line) => {
         const heading = document.createElement('h1');
-        heading.textContent = line
+        heading.append(line)
         return heading
     },
     '##': (line) => {
         const heading = document.createElement('h2');
-        heading.textContent = line
+        heading.append(line)
         return heading
     },
     '###': (line) => {
         const heading = document.createElement('h3');
-        heading.textContent = line
+        heading.append(line)
         return heading
     },
     '-': (line) => {
         const listItem = document.createElement('li');
-        listItem.textContent = line
+        listItem.append(line)
         return listItem;
     }
 }
@@ -30,11 +30,39 @@ function formatLine(line) {
 
     if (Object.keys(blockElements).includes(possibleBlockType)) {
         const cuttedParagraph = cuttedParagraphArray.slice(1).join(' ').trim()
-        const formattedElement = blockElements[possibleBlockType](cuttedParagraph)
-        return formattedElement
+
+        const formattedText = formatInline(cuttedParagraph)
+        return blockElements[possibleBlockType](formattedText)
     } else {
         const p = document.createElement('p');
-        p.textContent = line;
+        p.append(formatInline(line));
         return p;
     }
+}
+
+function formatInline(cuttedParagraph) {
+    
+    const formattedParagraphElements = [];
+    const splitParagraph = cuttedParagraph.split('**')
+    const lenght = splitParagraph.length;
+
+    formattedParagraphElements.push(splitParagraph[0])
+    for (let i = 1; i < lenght; i += 2) {
+        const slice = splitParagraph[i]
+        const e = document.createElement('b')
+        e.textContent = slice;
+        formattedParagraphElements.push(e)
+        // end of bold element ('**bold text>>**<<')
+        // save the next text fragment
+        formattedParagraphElements.push(splitParagraph[i + 1])
+    }
+
+    const formattedText = document.createElement('span');
+    for (let i = 0; i < formattedParagraphElements.length; i++) {
+        formattedText.append(
+            formattedParagraphElements[i]
+        )
+    }
+
+    return formattedText
 }
